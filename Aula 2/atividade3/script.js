09d30bd26afd18a714e4b1ceabbaed50972b777e
Luiz Eduardo <luizeduardosouzadecarvalho3@gmail.com>
2025-08-19 18:34:59 -0300

    Add files via upload

---
+++ Aula 2/atividade3/script.js
@@ -0,0 +1,15 @@
+let numero1 = 2
+let numero2 = 5
+let numero3 = 3
+
+if (numero1 > numero2 && numero1 > numero3){
+    console.log('Numero1 é maior')
+}
+
+else if (numero2 > numero1 && numero2 > numero3){
+    console.log('Numero2 é maior')
+}
+
+else{
+    console.log('Numero3 é maior')
+}
---
+++ Aula 2/atividade4/script.js
@@ -0,0 +1,16 @@
+       
+       
+       // ENTRADA
+       let numero = 5;
+
+       // PROCESSAMENTO
+       let resultado
+
+       if (numero % 2 == 0){
+        resultado = "Par"
+       }else{
+        resultado = "Impar"
+       }
+
+       //SAIDA
+       console.log(resultado)
